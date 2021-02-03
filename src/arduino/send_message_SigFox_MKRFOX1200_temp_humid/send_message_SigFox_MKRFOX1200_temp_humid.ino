7b6d95bfa4a41ea84684958082000c026ac94743
Alex Gallucci <54532975+Tortipouss@users.noreply.github.com>
2021-02-03 17:19:41 +0100

    Envoi la température et l'humidité au serveur sigFox

--- src/arduino/send_message_SigFox_MKRFOX1200_temp_humid/send_message_SigFox_MKRFOX1200_temp_humid.ino
+++ src/arduino/send_message_SigFox_MKRFOX1200_temp_humid/send_message_SigFox_MKRFOX1200_temp_humid.ino
@@ -5,59 +5,41 @@
 #define DHTTYPE DHT11   // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
-typedef struct __attribute__ ((packed)) sigfox_message { // see http://www.catb.org/esr/structure-packing/#_structure_alignment_and_padding
+typedef struct __attribute__ ((packed)) sigfox_message {
   float moduleTemperature;
-  float moduleHumidity;
+  int moduleHumidity;
 } SigfoxMessage;
 
 // stub for message which will be sent
 SigfoxMessage msg;
 
-// =================== UTILITIES ===================
-void reboot() {
-  NVIC_SystemReset();
-  while (1);
-}
-// =================================================
-
-
 void setup() {
   dht.begin();
   Serial.begin(115200);
-  while (!Serial);
+  while (Serial);
 
   if (!SigFox.begin()) {
     Serial.println("SigFox error, rebooting");
-    reboot();
   }
-
   delay(100); // Wait at least 30ms after first configuration
+}
 
-  float h = dht.readHumidity();
+void loop() {
+  int h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-
-
-  // Enable debug prints and LED indication
-  SigFox.debug();
 
   // Read and convert the module temperature
-  //msg.moduleTemperature = (int32_t) (SigFox.internalTemperature() * 100.0); // température 1/100th of degrees
-  msg.moduleTemperature = (float) (t); // température 1/100th of degrees
-  msg.moduleHumidity = (float) (h); // température 1/100th of degrees
+  msg.moduleTemperature = t; // température 1/100th of degrees
+  msg.moduleHumidity = h; // température 1/100th of degrees
 
 
 
   Serial.print("Temperature : ");
-  Serial.print(msg.moduleTemperature); // display what we will send in Hexadecimal
-  Serial.print(" | ");
-  Serial.print(msg.moduleHumidity); // display what we will send in Decimal
+  Serial.print(msg.moduleTemperature, HEX); // display what we will send in Hexadecimal
+  Serial.print("\nHumidité : ");
+  Serial.print(msg.moduleHumidity, HEX); // display what we will send in Decimal
 
   // Clears all pending interrupts
   SigFox.status();
@@ -65,9 +47,11 @@ void setup() {
 
   // Send the data
   SigFox.beginPacket();
-  SigFox.write((uint8_t*)&msg, sizeof(SigfoxMessage));;
+  SigFox.write((uint8_t*)&msg, sizeof(SigfoxMessage));
+
+  Serial.print("Status: ");
+  Serial.println(SigFox.endPacket());
 
   SigFox.end();
-}
-
-void loop() {}
+  delay(10000);
+  }
